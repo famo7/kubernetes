a402a939e918755bc4db601e89b819feb3d6818e
famo7 <famo1901@gmail.com>
2026-06-15 08:20:24 +0200

    2.1 log_output

--- log_output/log-reader/index.js
+++ log_output/log-reader/index.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const fs = require('fs');
+const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
 
 const logPath = '/usr/src/app/files/output.log';
-const counterPath = '/app/files/counter.txt';
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     const log = fs.existsSync(logPath)
         ? fs.readFileSync(logPath, 'utf8')
         : 'No logs yet';
 
-    const counter = fs.existsSync(counterPath)
-        ? fs.readFileSync(counterPath, 'utf8')
-        : '0';
+    const response = await axios.get('http://ping-pong-svc:2345/count');
+    const counter = response.data;
 
     res.send(`${log}\nPing / Pongs: ${counter}`);
 });
--- ping_pong/index.js
+++ ping_pong/index.js
@@ -1,19 +1,13 @@
 const express = require('express');
-const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const filePath = '/app/files/counter.txt';
-
-// Read counter from file on startup if it exists
 let counter = 0;
-if (fs.existsSync(filePath)) {
-    counter = parseInt(fs.readFileSync(filePath, 'utf8'));
-}
-
+app.get('/count', (req, res) => {
+    res.send(String(counter));
+});
 app.get('/pingpong', (req, res) => {
     counter++;
-    fs.writeFileSync(filePath, String(counter));
     res.send(`pong ${counter}`);
 });
 
