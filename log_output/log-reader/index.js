151236d748a3c37a1db7c24b20c1c090f6b6d21a
famo7 <famo1901@gmail.com>
2026-06-17 20:38:56 +0200

    2.5 log_output

--- log_output/log-reader/index.js
+++ log_output/log-reader/index.js
@@ -5,8 +5,15 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const logPath = '/usr/src/app/files/output.log';
+const infoPath = '/etc/config/information.txt';
 
 app.get('/', async (req, res) => {
+    const fileContent = fs.existsSync(infoPath)
+        ? fs.readFileSync(infoPath, 'utf8').trim()
+        : 'No file';
+
+    const message = process.env.MESSAGE || 'not set';
+
     const log = fs.existsSync(logPath)
         ? fs.readFileSync(logPath, 'utf8')
         : 'No logs yet';
@@ -14,7 +21,7 @@ app.get('/', async (req, res) => {
     const response = await axios.get('http://ping-pong-svc:2345/count');
     const counter = response.data;
 
-    res.send(`${log}\nPing / Pongs: ${counter}`);
+    res.send(`file content: ${fileContent}\nenv variable: MESSAGE=${message}\n${log}\nPing / Pongs: ${counter}`);
 });
 
 app.listen(port, () => {
