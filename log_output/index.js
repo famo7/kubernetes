6fecc2cbe0f3f47a007188ce6888fbb68f429829
famo7 <famo1901@gmail.com>
2026-07-08 11:16:12 +0200

    1.7 log-output

--- log_output/index.js
+++ log_output/index.js
@@ -1,5 +1,8 @@
+const express = require('express');
 const crypto = require('crypto');
 
+const app = express();
+const PORT = process.env.PORT || 3000;
 const randomString = crypto.randomUUID();
 
 console.log(`Application started. Generated string: ${randomString}`);
@@ -8,3 +11,12 @@ setInterval(() => {
   const timestamp = new Date().toISOString();
   console.log(`${timestamp}: ${randomString}`);
 }, 5000);
+
+app.get('/', (req, res) => {
+  const timestamp = new Date().toISOString();
+  res.send(`${timestamp}: ${randomString}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
